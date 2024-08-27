8f2b4d72947466e4f7439bd17b44d8d4c92399cc
José Gabriel Reis da Cruz <diouzedev@gmail.com>
2024-08-27 15:02:06 -0300

    Voto

---
+++ ex011/index.js
@@ -0,0 +1,8 @@
+var idade = 18
+if (idade < 16) {
+    console.log('Menor de idade')
+} else if (idade < 18 || idade > 65) {
+    console.log('O voto é opicional')
+} else if (idade >= 18){
+    console.log('Voto obrigatório')
+}
