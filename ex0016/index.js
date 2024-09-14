bcf777f8209be589752f2c6f6433d23b2f73c0d1
José Gabriel Reis da Cruz <diouzedev@gmail.com>
2024-09-14 14:58:44 -0300

    repetições

---
+++ ex0016/index.js
@@ -0,0 +1,25 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+}while (c <= 8)
+
+
+
+/*var c = 1
+while (c <= 8) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+/*
+console.log('Passo')
+console.log('Passo')
+console.log('Passo')
+console.log('Passo')
+console.log('Passo')
+console.log('Passo')
+console.log('Passo')
+console.log('Passo')
+*/
