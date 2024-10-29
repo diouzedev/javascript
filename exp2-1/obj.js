9a255cc80813c41e73c0cc99affaee1a76e277e1
José Gabriel Reis da Cruz <diouzedev@gmail.com>
2024-10-29 15:12:32 -0300

    To-do list part 1

---
+++ exp2-1/obj.js
@@ -0,0 +1,9 @@
+const dadosAlfredo = {
+    nome: 'Alfredo',
+    idade: 17,
+    endereco: 'Tiurusbango',
+    cidade: 'Chique chique',
+    cpf: 53454345600,
+}
+
+console.log(`Os dados seguintes são Nome: ${dadosAlfredo.nome}; Idade: ${dadosAlfredo.idade}; Endereço: ${dadosAlfredo.endereco}; Cidade: ${dadosAlfredo.cidade}; CPF:${dadosAlfredo.cpf}`)
---
+++ projetos-javascript/js/check.js
@@ -0,0 +1,3 @@
+
+
+const inputBox = document.querySelector(".linha")
